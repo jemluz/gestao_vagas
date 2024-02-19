dd8a7d08a4546d3cb74bcd799ee60dbcf2cabdf0
jemluz <contatojemima@gmail.com>
2024-02-19 18:29:50 -0300

    add: database table (candidate entity)
    - add @NotBlank to username validation
    - add createdAt field

--- src/main/java/com/ignite/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/ignite/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,20 +1,32 @@
 package com.ignite.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
   private String name;
 
+  @NotBlank
   @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaços.")
   private String username;
 
@@ -25,5 +37,8 @@ public class CandidateEntity {
   private String password;
   private String description;
   private String curriculum;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 
 }
