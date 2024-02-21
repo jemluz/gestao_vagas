0d50ea66093183bb3bd58a86c70120a0aae7071b
jemluz <contatojemima@gmail.com>
2024-02-21 14:14:16 -0300

    add: candidate repository
    - use Spring Data JPA repository structure
    - create repository instance into CandidateController

--- src/main/java/com/ignite/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/ignite/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -4,9 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ignite.gestao_vagas.modules.candidate.CandidateEntity;
+import com.ignite.gestao_vagas.modules.candidate.CandidateRepository;
 
 import jakarta.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -15,9 +17,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+  @Autowired
+  private CandidateRepository candidateRepository;
+
   @PostMapping("/")
-  public void create(@Valid @RequestBody CandidateEntity candidateEntity ) {
-    System.out.println(candidateEntity.getEmail());
+  public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity ) {
+    return this.candidateRepository.save(candidateEntity);
   }
 
 }
