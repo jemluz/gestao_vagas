75c0a7518b648e63b803b89aedf79befb2bc48f5
jemluz <contatojemima@gmail.com>
2024-02-21 21:31:33 -0300

    add: company entity
    - create company entity file
    - use `@Entity` to define/create table (postgres)
    - use `@Data` to define getters and setters
    - use `@Id` and `@GeneratedValue` to setup primary key (id) column in database
    - implementation of fields validation (company_name, email, password)

---
+++ src/main/java/com/ignite/gestao_vagas/modules/company/CompanyEntity.java
@@ -0,0 +1,42 @@
+package com.ignite.gestao_vagas.modules.company;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Entity(name = "company")
+@Data
+public class CompanyEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @NotBlank
+  @Pattern(regexp = "\\S+", message = "O campo [company_name] não deve conter espaço")
+  private String company_name;
+
+  @Email(message = "O campo [email] deve conter um e-mail válido")
+  private String email;
+
+  @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
+  private String password;
+
+  private String name;
+  private String website;
+  private String description;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+}
