d85cfc8f0595d8f2ff65a22d98eef601d26440e4
jemluz <contatojemima@gmail.com>
2024-03-06 09:41:07 -0300

    add: route's permissions
    - allow all /candidate routes (only post)
    - allow all /company routes (only post)
    - all /job routes need auth

--- src/main/java/com/ignite/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/com/ignite/gestao_vagas/security/SecurityConfig.java
@@ -8,9 +8,24 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-  // @Bean
-  // SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-  //   http.csrf(csrf -> csrf.disable());
-  //   return http.build();
-  // }
+  @Bean
+  SecurityFilterChain securityFilterChain(
+    HttpSecurity http
+  ) throws Exception {
+
+    http.csrf(csrf -> csrf.disable())
+      .authorizeHttpRequests(
+        auth -> {
+
+          auth.requestMatchers("/candidate/").permitAll()
+            .requestMatchers("/company/").permitAll();
+
+          auth.anyRequest().authenticated();
+
+        }
+      );
+
+    return http.build();
+
+  }
 }
